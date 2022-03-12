0ffacfbf17ab5b1d6cb5ca915fdfb2faefa970fe
bappy <bappykhan226@gmail.com>
2022-03-12 20:58:27 +0600

    update

--- script.js
+++ script.js
@@ -39,27 +39,22 @@ $(document).ready(function(){
 
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+var btn = $('#button');
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
   } else {
-    mybutton.style.display = "none";
+    btn.removeClass('show');
   }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+});
 
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+
+
